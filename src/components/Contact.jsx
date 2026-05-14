c39267c164dd070f389a196439ed415a2d36d4cc
muhamadtegarputrapratama <muhamadtegarputrapratama@smkwikrama.sch.id>
2026-05-15 05:41:17 +0700

    delete unused motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion, useInView } from "framer-motion";
+import {  useInView } from "framer-motion";
 import { useRef } from "react";
 import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope, FaInstagram } from "react-icons/fa";
 import emailjs from "@emailjs/browser";
